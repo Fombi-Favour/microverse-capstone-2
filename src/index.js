bb66a3f4fac2f5b6a5e7ad8beca4a8f394f3676c
Fombi-Favour <fombifavour@gmail.com>
2023-06-28 16:11:08 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css';
 import Logo from './asset/logo.png';
+import Pics from './asset/bg-all.webp';
 
 const logo = document.getElementById('logo');
 logo.src = Logo;
+
+const pic = document.getElementById('pic');
+pic.src = Pics;
