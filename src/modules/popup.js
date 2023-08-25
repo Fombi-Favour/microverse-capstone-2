28233abba51d8e4351a4edbf826d5e749fe30710
Fombi-Favour <fombifavour@gmail.com>
2023-08-25 13:10:28 +0100

    Delete popup.js

--- src/modules/popup.js
+++
@@ -1,9 +0,0 @@
-const blur = document.getElementById('blur');
-const popComment = document.getElementById('pop-comment');
-
-const toggle = () => {
-  blur.classList.toggle('active');
-  popComment.classList.toggle('active');
-};
-
-export default toggle;
