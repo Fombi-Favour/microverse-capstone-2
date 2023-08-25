73e665ac7b1552bcddf6af844dce02f4d5ed2850
Fombi-Favour <fombifavour@gmail.com>
2023-08-25 13:10:25 +0100

    Update display.js

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
-import { getPokemonUrl } from './getData.js';
+import { getPokemonSpecies, getPokemonUrl } from './getData.js';
 import { likeApi } from './apiUrl.js';
-import toggle from './popup.js';
+import commentModals from './modals.js';
 
 const displayPokemon = async () => {
   const pokemon = await getPokemonUrl();
+  const url = await getPokemonSpecies();
+  console.log(url[0].habitat.name);
   const numberLikes = JSON.parse(localStorage.getItem('likes'));
   const postLikes = Array.isArray(numberLikes) ? numberLikes : [];
   let totalLikes;
   const card = document.querySelector('.pokemon');
   card.innerHTML = '';
+  // desktop version of pokemon count
+  const count = document.querySelector('.pokemon-count');
+  count.innerHTML = `(${pokemon.length})`;
+
+  // mobile version of pokemon count
+  const counter = document.querySelector('#mobile-count');
+  counter.innerHTML = `(${pokemon.length})`;
+
   pokemon.forEach((data) => {
     // eslint-disable-next-line no-return-assign
     postLikes.forEach((like) => (parseInt(like.item_id, 10) === data.id ? (totalLikes = like.likes) : ''));
@@ -18,31 +28,23 @@ const displayPokemon = async () => {
       .get(likeApi)
       .then((res) => localStorage.setItem('likes', JSON.stringify(res.data)));
     const display = `
-            <div class="pokemon-content">
-                <img id="pic" src="${
-  data.sprites.other['official-artwork'].front_default
-}" alt="pokemon-pic">
-                <div class="content-desc">
-                    <h4>${data.species.name}</h4>
-                    <div class="likes">
-                        <button type="button" class="like-me" id=${data.id}>
-                          <i class="las la-heart"></i>
-                        </button>
-                        <span class="like-count">${
-  totalLikes || '0'
-} Likes</span>
-                    </div>
-                </div>
-                <div class="action">
-                    <button type="button" class="btn-comment" id="${
-  data.species.name
-}">Comments</button>
-                    <button type="button" class="btn-reserve" id="${
-  data.species.name
-}">Reserve</button>
+        <div class="pokemon-content">
+            <img id="pic" src="${data.sprites.other['official-artwork'].front_default}" alt="pokemon-pic">
+            <div class="content-desc">
+                <h4>${data.species.name}</h4>
+                <div class="likes">
+                    <button type="button" class="like-me" id=${data.id}>
+                      <i class="las la-heart"></i>
+                    </button>
+                    <span class="like-count">${totalLikes || '0'} Likes</span>
                 </div>
             </div>
-        `;
+            <div class="action">
+                <button type="button" class="btn-comment" id="${data.species.name}">Comments</button>
+                <button type="button" class="btn-reserve" id="${data.species.name}">Reserve</button>
+            </div>
+        </div>
+    `;
     card.innerHTML += display;
 
     const likeButton = document.querySelectorAll('.like-me');
@@ -62,20 +64,23 @@ const displayPokemon = async () => {
     liking();
   });
 
-  // desktop version of pokemon count
-  const count = document.querySelector('.pokemon-count');
-  count.innerHTML = `(${pokemon.length})`;
+  const blur = document.getElementById('blur');
+  const popComment = document.getElementById('pop-comment');
 
-  // mobile version of pokemon count
-  const counter = document.querySelector('#mobile-count');
-  counter.innerHTML = `(${pokemon.length})`;
+  const toggle = () => {
+    blur.classList.toggle('active');
+    popComment.classList.toggle('active');
+  };
 
-  const commentButton = document.querySelector('.btn-comment');
+  const commentButton = document.querySelectorAll('.btn-comment');
   // const reserveButton = document.querySelectorAll('.btn-reserve');
   const closeButton = document.querySelector('.close');
 
-  commentButton.addEventListener('click', () => {
-    toggle();
+  commentButton.forEach((item) => {
+    item.addEventListener('click', async (e) => {
+      await commentModals(pokemon, e.target.dataset.id);
+      toggle();
+    });
   });
 
   closeButton.addEventListener('click', () => {
