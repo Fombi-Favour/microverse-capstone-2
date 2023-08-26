9c032b3de01ebc9e2c273850f3586b6e8cfbb96f
Fombi-Favour <fombifavour@gmail.com>
2023-08-27 00:24:58 +0100

    Update apiUrl.js

--- src/modules/apiUrl.js
+++ src/modules/apiUrl.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export const displayApi = 'https://pokeapi.co/api/v2/pokemon/';
-export const likeApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/24jUJG2gpRmmZUOOghxZ/likes';
-export const commentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/24jUJG2gpRmmZUOOghxZ/comments/';
-export const reserveApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/24jUJG2gpRmmZUOOghxZ/reservations';
+export const likeApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/2Q7WAagXF8IG8teNMkK7/likes';
+export const commentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/2Q7WAagXF8IG8teNMkK7/comments/';
+export const reserveApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/2Q7WAagXF8IG8teNMkK7/reservations';
